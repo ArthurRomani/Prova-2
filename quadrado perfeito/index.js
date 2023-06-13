cbe1b7aa74d7bab355b0828b7d3751671ce5d8c8
Arthur Romani Inacio de Souza <125612826+ArthurRomani@users.noreply.github.com>
2023-06-13 10:21:01 -0300

    Add files via upload

--- quadrado perfeito/index.js
+++ quadrado perfeito/index.js
@@ -1,12 +1,25 @@
-function verificaQuadradoPerfeito(numero) {
-    
-    var raiz = Math.sqrt(numero);
-    
-// Verificando  se a raiz quadrada é um número inteiro
-    if (Number.isInteger(raiz)) {
-      return true;
-    } else {
-      return false;
-    }
-  } 
-  
+//Verificar se e quadrado perfeito
+
+function quadradoPerfeito(numero) {
+  var raiznumero = Math.floor(Math.sqrt(numero));
+  return raiznumero * raiznumero === numero;
+}
+
+function verificarQuadradoPerfeito() {
+  var num = prompt("Insira um número");
+
+  if (isNaN(num)) {
+    alert("Por favor, insira um número válido.");
+    return;
+  }
+
+  num = parseInt(num);
+
+  if (quadradoPerfeito(num)) {
+    alert(num + " é um quadrado perfeito.");
+  } else {
+    alert(num + " não é um quadrado perfeito.");
+  }
+}
+
+//Para verificar basta colar o codigo no console e depois usar o "console.log(verificarQuadradoPerfeito());"
